3c3d25606092978193aec925af9dd4f2bba3afcc
Shiva Kumar Karru <103453725+Shivaksk119@users.noreply.github.com>
2024-12-26 00:45:39 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,24 @@
 //your JS code here. If required.
+let timer = document.getElementById('timer');
+
+const updateDateTime=()=>{
+	let date = new Date();
+
+	let day = date.getDate();
+	let month = date.getMonth();
+	let year = date.getFullYear();
+	
+	let hrs = date.getHours();
+	let min = date.getMinutes() < 10 ? '0'+date.getMinutes() : date.getMinutes();
+	let sec = date.getSeconds() < 10 ? '0'+date.getSeconds() : date.getSeconds();
+	let merdian = hrs >= 12 ? 'PM':'AM';
+	if(hrs>12) hrs = hrs-12;
+	if(hrs===0) hrs = 12;
+
+	let ans = `${day}/${month}/${year}, ${hrs}:${min}:${sec} ${merdian}`;
+
+	timer.innerText = ans;
+	
+}
+
+setInterval(updateDateTime, 1000)
